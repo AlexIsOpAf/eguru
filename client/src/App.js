498c72f849a81fd5a8b627ab5bfabf64eb6469b5
AlexIsOpAf <truearchy@gmail.com>
2021-11-25 06:42:04 +0000

    Unit tests are a joke in JS

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import IRoutes from "./Routes";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import WithLayout from "WithLayout";
+
+import DashboardPage from "./views/Dashboard/Dashboard.js";
+import UserProfile from "./views/UserProfile/UserProfile.js";
+import Notifications from "./views/Notifications/Notifications";
+
+import { Main as MainLayout, Dashboard as DashboardView } from "./layouts";
+
+import { LoginSimple as LoginView } from "./views/authPages";
+
+// Landing pages
+import {
+  Home as HomeView,
+} from "./views/landingPages";
+
 
 import "react-lazy-load-image-component/src/effects/blur.css";
 import "slick-carousel/slick/slick.css";
@@ -15,7 +29,23 @@ import "scss/slick-slider.scss";
 const App = () => {
   return (
     <Router>
-      <IRoutes />
+      <Routes>
+        <Route
+          path="/"
+          element={<WithLayout component={HomeView} layout={MainLayout} />}
+        />
+
+        <Route
+          path="/login"
+          element={<WithLayout component={LoginView} layout={MainLayout} />}
+        />
+
+        <Route path="/admin" element={<DashboardView />}>
+          <Route path="overview" element={<DashboardPage />} />
+          <Route path="user" element={<UserProfile />} />
+          <Route path="questions" element={<Notifications />} />
+        </Route>
+      </Routes>
     </Router>
   );
 };
