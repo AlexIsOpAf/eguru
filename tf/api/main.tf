d90cb95fa4073369de6c3e9068805a001050cbf0
AlexIsOpAf <truearchy@gmail.com>
2021-11-28 06:12:47 +0000

    Updated Terraform: ECR, VPC, Subnets

--- tf/api/main.tf
+++ tf/api/main.tf
@@ -13,7 +13,99 @@ terraform {
 }
 
 # Configure the AWS Provider
-provider "aws" {}
+provider "aws" {
+  region = "eu-west-2"
+}
+
+
+
+# ------------------------------------------------------------------------------
+# CONFIGURE OUR AWS ECR
+# ------------------------------------------------------------------------------
+module "ecr" {
+
+  source = "./terraform-aws-ecr"
+
+  name                 = "eguru"
+  scan_on_push         = true
+  timeouts_delete      = "60m"
+  image_tag_mutability = "IMMUTABLE"
+
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "repo policy",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:DescribeRepositories",
+                "ecr:GetRepositoryPolicy",
+                "ecr:ListImages",
+                "ecr:DeleteRepository",
+                "ecr:BatchDeleteImage",
+                "ecr:SetRepositoryPolicy",
+                "ecr:DeleteRepositoryPolicy"
+            ]
+        }
+    ]
+}
+EOF
+
+  # Only one lifecycle policy can be used per repository.
+  # To apply multiple rules, combined them in one policy JSON.
+  lifecycle_policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire untagged images older than 14 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Keep last 30 dev images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["dev"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 30
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+
+  # Tags
+  tags = {
+    Owner       = "eguru_prod"
+    Environment = "prod"
+    Terraform   = true
+  }
+
+}
+
+
 
 
 # ------------------------------------------------------------------------------
@@ -30,7 +122,9 @@ resource "aws_vpc" "eguru_vpc" {
 }
 
 
-
+# ------------------------------------------------------------------------------
+# CONFIGURE OUR AWS SUBNET
+# ------------------------------------------------------------------------------
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.eguru_vpc.id
   cidr_block = "10.0.0.0/28"
